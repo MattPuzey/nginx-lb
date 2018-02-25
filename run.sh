bf3e8c395954f6c7684ef0a159b2d2ea2d7f93cb
matthew.puzey <matthew.puzey@nhs.net>
2018-02-25 12:47:32 +0000

    try to isolate puppet logs for deubugging container on docker run

--- run.sh
+++ run.sh
@@ -14,7 +14,9 @@ function runLoadBalancer() {
     pushd web
     docker build . --rm -t web
     popd
-    docker run -it --publish 32768:80 --name nginx nginx
+    #docker run -td --publish 32768:80 --name nginx nginx
+    docker run -it --publish 32768:80 --name nginx nginx sh -c "while true; do $(echo date); sleep 1; done"
+
 }
 
 function clearIntermediateImages() {
