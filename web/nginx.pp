6ed5391335a0ad011b4416ee8b3195af37cc075d
matthew.puzey <matthew.puzey@nhs.net>
2018-02-25 18:19:41 +0000

    server static hello world from nginx

--- web/nginx.pp
+++ web/nginx.pp
@@ -7,3 +7,42 @@ service { "nginx":
     ensure => running,
     enable => true
 }
+
+file { "/etc/nginx/sites-enabled/default":
+    require => Package["nginx"],
+    ensure  => absent,
+    notify  => Service["nginx"]
+}
+
+file { "/www":
+    ensure => "directory"
+}
+file { "/www/index.html":
+    require => File["/www"],
+    ensure => "file",
+    content => "<!DOCTYPE html>
+        <html><body>
+        Hello, world.
+        "
+}
+
+file { "/etc/nginx/sites-available/puppet-demo":
+    require => [
+        Package["nginx"],
+        File["/www"]
+    ],
+    ensure => "file",
+    content =>
+        "server {
+            listen 80 default_server;
+            server_name _;
+            location / { root /www; }
+        }",
+    notify => Service["nginx"]
+}
+file { "/etc/nginx/sites-enabled/puppet-demo":
+    require => File["/etc/nginx/sites-available/puppet-demo"],
+    ensure => "link",
+    target => "/etc/nginx/sites-available/puppet-demo",
+    notify => Service["nginx"]
+}
